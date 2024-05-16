9a5196fe39d533f3037f0314b216e202d0e360e6
Sathvara Raj <109301347+Raj5222@users.noreply.github.com>
2024-05-16 22:44:27 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -54,6 +54,6 @@ app.post('/', upload.single('image'), async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
